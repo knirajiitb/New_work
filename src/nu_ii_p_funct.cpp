d0696fedd576ff7c428fa1030bac84aa27b69343
knirajiitb <80027702+knirajiitb@users.noreply.github.com>
2021-08-04 23:10:53 +0530

    Update nu_ii_p_funct.cpp

--- src/nu_ii_p_funct.cpp
+++ src/nu_ii_p_funct.cpp
@@ -8,7 +8,7 @@ double nu_ii_p_funct(double k, int counter, double beta_constant, double epsilon
 	double A= 0.5*(2 + (beta_constant*beta_constant/(k*k)));
 	double B= abs((A+1)/(A-1));
 	
-	double iiA= (pow(e,4)*m_h* abs(N_imp))/(32*pi*pow(k,3)*epsilon_s*epsilon_s*epsilon_0*epsilon_0*pow(h_bar,3));
+	double iiA= (pow(e,4)*m_h* abs(N_im))/(32*pi*pow(k,3)*epsilon_s*epsilon_s*epsilon_0*epsilon_0*pow(h_bar,3));
 	double iiB= ((3*A -1)*(3*A -1)*log(B) - 18*A +12 -8/(A+1));
 	
 	double ii_p= iiA*iiB ;
