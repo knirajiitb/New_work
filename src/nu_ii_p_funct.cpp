55d2b2aece92e276e8a314c042bb821aaa6a33e7
knirajiitb <80027702+knirajiitb@users.noreply.github.com>
2021-08-07 15:20:20 +0530

    Update nu_ii_p_funct.cpp

--- src/nu_ii_p_funct.cpp
+++ src/nu_ii_p_funct.cpp
@@ -8,7 +8,7 @@ double nu_ii_p_funct(double k, int counter, double beta_constant, double epsilon
 	double A= 0.5*(2 + (beta_constant*beta_constant/(k*k)));
 	double B= abs((A+1)/(A-1));
 	
-	double iiA= (pow(e,4)*m_h* N_im)/(32*pi*pow(k,3)*epsilon_s*epsilon_s*epsilon_0*epsilon_0*pow(h_bar,3));
+	double iiA= (pow(e,4)*m_h* N_ii)/(32*pi*pow(k,3)*epsilon_s*epsilon_s*epsilon_0*epsilon_0*pow(h_bar,3));
 	double iiB= ((3*A -1)*(3*A -1)*log(B) - 18*A +12 -8/(A+1));
 	
 	double ii_p= iiA*iiB ;
@@ -16,7 +16,7 @@ double nu_ii_p_funct(double k, int counter, double beta_constant, double epsilon
 	FILE *fid1;
 	fid1= fopen("nu_iip.dat","w");
 	for(int i=0;i<points;i++){
-		fprintf(fid1, energy_n[i], ii_p);
+		fprintf(fid1,"%e     %e\n", energy_n[i], ii_p);
 	}
 	fclose(fid1);
 		
