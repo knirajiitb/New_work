4c9dc4dbfae20df17eb457ffd8e8a22960fac8c3
knirajiitb <80027702+knirajiitb@users.noreply.github.com>
2021-08-15 23:44:41 +0530

    Update nu_de_p_funct.cpp

--- src/nu_de_p_funct.cpp
+++ src/nu_de_p_funct.cpp
@@ -5,5 +5,32 @@
 void nu_de_p_funct()
 {
 	
-}
+	
+	// from ramu thesis equation 3.32a 
+	//double adp= (k_B*T*pow(E_deformation,2)*m_h*k)/(2*pi*pow(h_bar,3)*C_long);
+	double T;
+	double v;
+	double k_dum;
+	double nu_deform[limit2]={0};
+	cout<<"trial,I'M getting compiled"<<endl;
+	
 
+	for (int counter = 0;counter<points;counter++)
+	    		{
+			        k_dum = k_grid[counter];
+			        v= v_p[counter];					
+	            		//nu_deform[counter] = nu_de_p_funct(k_dum,T);
+	            		nu_deform[counter] = (k_B*T*pow(E_deformation,2)*k_dum*k_dum)/(2*pi*h_bar*h_bar*C_long*v)*1e10*1.60217657/1e8;
+				//cout<<"nu_deformation_p[counter] =  "<<nu_deform[counter]<<endl;
+			}
+	
+		FILE *fid1;
+		fid1= fopen("ADPscatt.dat","w");
+			for(int i=0;i<points;i++){
+			fprintf(fid1,"%e     %e\n", energy_p[i], nu_deform[i]);
+			}
+		fclose(fid1);
+			
+		
+	
+}
