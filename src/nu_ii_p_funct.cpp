ba33b22a88c4f7fba1919422d6d913ed69018c2b
knirajiitb <80027702+knirajiitb@users.noreply.github.com>
2021-08-18 17:26:15 +0530

    Update nu_ii_p_funct.cpp

--- src/nu_ii_p_funct.cpp
+++ src/nu_ii_p_funct.cpp
@@ -2,85 +2,89 @@
 
 
 // ionized imprity scattering calculation //---- equation 3.29 of Ramu Thesis
-void nu_ii_p_funct()
+void nu_ii_p_funct(int T_loop)
 {
 	
 	double v;
 	double k_dum;
-	double ii_p[limit2]={0};
+
 	double A;
 	double B;
 	double iiB;
 	double iiA;
-	double epsilon_lf= epsilon_s[0];
-	double beta_constant =  0.140607315390570;
+	double epsilon_lf= epsilon_s[T_loop];
 	
-	/*    // This segment is from beta.cpp to calculate inverse screening length for p-type
-	double T;
-	int T_loop;
-	double beta_constant;
-	double k , de, k_step;
-    	double integral = 0;
-
-	    for(int counter = 0;counter <= points-2; counter++)
+	double T= T_array[T_loop];
+	
+	double beta_const;
+	double de_p, k_step_p;
+    	double integral_1 = 0;
+	
+	
+	
+	for(int counter = 0;counter <= points-2; counter++)
 	    {
-		k = k_grid[counter];
-		de = energy_p[counter+1]-energy_p[counter];
-		k_step = k_grid[counter+1]-k_grid[counter];
-
+		k_dum = k_grid[counter];
+		de_p = energy_p[counter+1]-energy_p[counter];
+		//cout<<"de_p= "<<de_p<<endl;
+		k_step_p = k_grid[counter+1]-k_grid[counter];
+		//cout<<"k_step = "<<k_step_p<<endl;
+		
 		if (free_e ==0)  // DOSCAR is used
 		{
-		   integral = integral+ de*((1e6*Ds_p[counter])/volume1)*f0(energy_p[counter],efef_p,T)*(1-f0(energy_p[counter],efef_p,T));
-		    
-		    // unit is (1/m)^3; 1e6 is multiplied to convert volume from (1/cm)^3 to (1/m)^3
+		   integral_1 = integral_1+ de_p*((1e6*Ds_p[counter])/volume1)*f0(energy_p[counter],efef_p,T)*(1-f0(energy_p[counter],efef_p,T));
+		   //cout<< "integral doscar = "<<integral_1<<endl;
+		   // unit is (1/m)^3; 1e6 is multiplied to convert volume from (1/cm)^3 to (1/m)^3
 		}
 		else
 		{
-		    integral = integral+k_step*(k/pi)*(k/pi)*f0(energy_p[counter],efef_p,T)*(1-f0(energy_p[counter],efef_p,T))*1e27;
+		    integral_1 = integral_1+k_step_p*(k_dum/pi)*(k_dum/pi)*f0(energy_p[counter],efef_p,T)*(1-f0(energy_p[counter],efef_p,T))*1e27;
+		    //cout<<"integral k_step = "<<integral_1<<endl;
 		    // unit is (1/m)^3
 		    // Part of equation (70) or Rode's book (book8)
 		}
 		
-	    }
+	     }
+	    
     
-		    double bet = (e*e/(epsilon_s[T_loop]*epsilon_0*(k_B*e)*T)*integral);   // unit (1/m)^2
-		    											// 	
-		    bet = pow(bet,0.5);   // unit (1/m) 
+	double beta = (e*e/(epsilon_s[T_loop]*epsilon_0*(k_B*e)*T)*integral_1);   // unit (1/m)^2		    								
+	beta_const = pow(beta,0.5);   // unit (1/m) 
 		    
-		    beta_constant = bet*1e-9;
-		    //converted from 1/m to 1/nm
-		    cout<<"screening length is ="<<beta_constant<<endl;
-		    */
-			    
-		for (int counter = 0;counter<points;counter++)
-	    		{
-			        k_dum = k_grid[counter];
-			        v= v_p[counter];
-				A= 0.5*(2 + (beta_constant*beta_constant/(k_dum*k_dum)));
+	//beta_const = beta_const*1e-9;
+	//converted from 1/m to 1/nm
+		   
+	cout<<"screening length is ="<<beta_const<<endl;
+		    
+	for (int counter = 0;counter<points;counter++)
+	   {
+		k_dum = k_grid[counter];
+		v= v_p[counter];
+		
+		A= 0.5*(2 + (beta_const*beta_const/(k_dum*k_dum)));		
+		B= abs((A+1)/(A-1));
 				
-				B= abs((A+1)/(A-1));
+		//cout<<"A = "<<A<<endl;
+		//cout<<"B = "<<B<<endl;
 				
-				//cout<<"A = "<<A<<endl;
-				//cout<<"B = "<<B<<endl;
+		iiA= (pow(e,4)* abs(N_ii))/(32*pi*k_dum*k_dum*epsilon_0*epsilon_lf*epsilon_lf*epsilon_0*v*pow(h_bar,2));
+		iiA= iiA*10e-10/(e*e);			// 10e-10/e*e  is for unit conversion
+		
+		iiB = abs((3*A -1)*(3*A -1)*log(B) - 18*A +12 -8/(A+1));
 				
-				iiA= (pow(e,4)* abs(N_ii))/(32*pi*k_dum*k_dum*epsilon_0*epsilon_lf*epsilon_lf*epsilon_0*v*pow(h_bar,2));
-				iiB = ((3*A -1)*(3*A -1)*log(B) - 18*A +12 -8/(A+1));
+		nu_ionizedimpurity_p[counter][0][0]= iiA*iiB  ; 
+		//cout<<"counter = "<<counter<<endl;		
 				
-				ii_p[counter]= iiA*iiB*1e27 ;
+	     }
 				
-				
-				
-				}
-				
-				
-				FILE *fid1;
-				fid1= fopen("iipScatt.dat","w");
-				for(int i=0;i<points;i++){
-					fprintf(fid1,"%e     %e\n", energy_p[i], ii_p[i]);
-				}
-				
-								
-				fclose(fid1);
+	// saving results
+	FILE *fid1;
+	fid1= fopen("iipScatt.dat","w");
+	fprintf(fid1,"#energy              nu_ii_p\n");
+	for(int i=0;i<points;i++)
+	{
+		fprintf(fid1,"%e     %e\n", energy_p[i], nu_ionizedimpurity_p[i][0][0]);
+	}
+	fclose(fid1);
 		
 				
 	
